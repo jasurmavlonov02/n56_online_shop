86979bb01001f0c55ab1e9b08ee284115a6c58c2
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2025-01-21 19:05:02 +0500

    models done!

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = "categories"
+
+
+class Product(BaseModel):
+    class RatingChoice(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=14, decimal_places=2)
+    discount = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='media/products/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
