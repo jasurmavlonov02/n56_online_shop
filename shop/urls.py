75e1dc563cc4cd06ba9ec495c0ed6ba2e7e5710f
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2025-01-23 19:15:32 +0500

    product detail

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ from shop import views
 urlpatterns = [
     path('', views.index, name='products'),
     path('detail/<int:pk>/', views.product_detail, name='product_detail'),
+    path('category-detail/<int:category_id>/', views.index, name='products_of_category')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,24 @@
+from typing import Optional
+
 from django.shortcuts import render, get_object_or_404
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 # Create your views here.
 
 
-def index(request):
-    products = Product.objects.all()
+def index(request, category_id: Optional[int] = None):
+    categories = Category.objects.all()
+
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
     context = {
-        'products': products
+        'products': products,
+        'categories': categories,
     }
     return render(request, 'shop/index.html', context=context)
 
